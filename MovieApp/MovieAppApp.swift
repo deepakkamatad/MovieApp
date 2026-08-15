492c62910ea97302e13d460658fb11eb12851803
deepak_atsolera <deepak@solera.com>
2026-08-15 12:58:58 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Deepak Basavaraj Kamatad on 15/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
